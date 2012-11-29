a8ddbb70df1021ab3f68d25057b56a7483e13fad
vf <vf_info@mail.ru>
2012-11-29 23:19:06 +0400

    fix shutdown

--- EchoServer/Program.cs
+++ EchoServer/Program.cs
@@ -137,7 +137,7 @@ namespace EchoServer
 
 			//CreateSomeGarbage(serversManager);
 
-			for (int i = 0; i < 240; i++)
+			for (int i = 0; i < 20; i++)
 			{
 				if (EventArgsManager.Created == EventArgsManager.Queued)
 					break;
--- SocketServer/SocketEx.cs
+++ SocketServer/SocketEx.cs
@@ -8,44 +8,45 @@ using System.Net.Sockets;
 
 namespace SocketServers
 {
-	static class SocketEx
-	{
-		public static void SafeShutdownClose(this Socket socket)
-		{
-			try
-			{
-				try
-				{
-					socket.Shutdown(SocketShutdown.Both);
-				}
-				catch { }
+    static class SocketEx
+    {
+        public static void SafeShutdownClose(this Socket socket)
+        {
+            try
+            {
+                try
+                {
+                    if (socket.Connected)
+                        socket.Shutdown(SocketShutdown.Both);
+                }
+                catch { }
 
-				socket.Close();
-			}
-			catch (ObjectDisposedException)
-			{
-			}
-		}
+                socket.Close();
+            }
+            catch (ObjectDisposedException)
+            {
+            }
+        }
 
-		public static void SendAsync(this Socket socket, ServerAsyncEventArgs e, ServerAsyncEventArgs.CompletedEventHandler handler)
-		{
-			e.Completed = handler;
-			if (socket.SendAsync(e) == false)
-				e.OnCompleted(socket);
-		}
+        public static void SendAsync(this Socket socket, ServerAsyncEventArgs e, ServerAsyncEventArgs.CompletedEventHandler handler)
+        {
+            e.Completed = handler;
+            if (socket.SendAsync(e) == false)
+                e.OnCompleted(socket);
+        }
 
-		public static void ConnectAsync(this Socket socket, ServerAsyncEventArgs e, ServerAsyncEventArgs.CompletedEventHandler handler)
-		{
-			e.Completed = handler;
-			if (socket.ConnectAsync(e) == false)
-				e.OnCompleted(socket);
-		}
+        public static void ConnectAsync(this Socket socket, ServerAsyncEventArgs e, ServerAsyncEventArgs.CompletedEventHandler handler)
+        {
+            e.Completed = handler;
+            if (socket.ConnectAsync(e) == false)
+                e.OnCompleted(socket);
+        }
 
-		public static void AcceptAsync(this Socket socket, ServerAsyncEventArgs e, ServerAsyncEventArgs.CompletedEventHandler handler)
-		{
-			e.Completed = handler;
-			if (socket.AcceptAsync(e) == false)
-				e.OnCompleted(socket);
-		}
-	}
+        public static void AcceptAsync(this Socket socket, ServerAsyncEventArgs e, ServerAsyncEventArgs.CompletedEventHandler handler)
+        {
+            e.Completed = handler;
+            if (socket.AcceptAsync(e) == false)
+                e.OnCompleted(socket);
+        }
+    }
 }
